71a9f211912536c1db99de51fd3c017c468294c4
Antonyos milad fahim necola <antonyosmilad60@gmail.com>
2025-09-17 21:54:59 +0300

    update

---
+++ src/components/Footerc.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footerc() {
+  return (
+    <footer>
+        <p>this is footer</p>
+    </footer>
+  )
+}
+
+export default Footerc
---
+++ src/components/Navc.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navc() {
+  return (
+    
+    <nav style={{backgroundColor: 'lightblue'}}>
+        <ul>
+            <li>About</li>
+
+            <li>Services</li>
+
+            <li>Contact</li>
+
+            <li>Blog</li>
+        </ul>
+    </nav>
+
+  )
+}
+
+export default Navc
